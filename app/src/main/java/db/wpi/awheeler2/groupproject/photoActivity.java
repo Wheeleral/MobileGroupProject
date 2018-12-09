30ea96971ad23d51336901c9e668c6f1989af1e9
Erika <essnow@wpi.edu>
2018-12-09 17:55:36 -0500

    yay catsgit add -u!

--- app/src/main/java/db/wpi/awheeler2/groupproject/photoActivity.java
+++ app/src/main/java/db/wpi/awheeler2/groupproject/photoActivity.java
@@ -8,6 +8,7 @@ import android.os.Debug;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Display;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -59,9 +60,9 @@ public class photoActivity extends AppCompatActivity {
         for(int i = 0; i < imagesOfAnimal.size(); i++) {
             System.out.print("entered for loop");
             ImageView image = new ImageView(this);
-            image.setLayoutParams(new android.view.ViewGroup.LayoutParams(80,60));
-            image.setMaxHeight(20);
-            image.setMaxWidth(20);
+            image.setLayoutParams(new android.view.ViewGroup.LayoutParams(500,500));
+            image.setMaxHeight(500);
+            image.setMaxWidth(500);
 
             // Adds the view to the layout
             layout.addView(image);
@@ -73,23 +74,23 @@ public class photoActivity extends AppCompatActivity {
 
     private void setPic(ImageView image, Bitmap bitmap) { // I feel like we should just be passing the filepath instead of the entire bitmap
         // Get the dimensions of the View
-        int targetW = image.getWidth();
-        int targetH = image.getHeight();
+        //int targetW = image.getWidth();
+        //int targetH = image.getHeight();
 
         // Get the dimensions of the bitmap
-        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
-        bmOptions.inJustDecodeBounds = true;
+       // BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        //bmOptions.inJustDecodeBounds = true;
         //BitmapFactory.decodeFile(path, bmOptions);  //hopefully putting this back in
-        int photoW = bmOptions.outWidth;
-        int photoH = bmOptions.outHeight;
+       // int photoW = bmOptions.outWidth;
+        //int photoH = bmOptions.outHeight;
 
         // Determine how much to scale down the image
-        int scaleFactor = Math.min(photoW/targetW, photoH/targetH);
+        //int scaleFactor = Math.min(photoW/targetW, photoH/targetH);
 
         // Decode the image file into a Bitmap sized to fill the View
-        bmOptions.inJustDecodeBounds = false;
-        bmOptions.inSampleSize = scaleFactor;
-        bmOptions.inPurgeable = true;
+        //bmOptions.inJustDecodeBounds = false;
+       // bmOptions.inSampleSize = scaleFactor;
+       // bmOptions.inPurgeable = true;
 
         //Bitmap bitmap = BitmapFactory.decodeFile(path, bmOptions);  //hopefully putting this back in
         image.setImageBitmap(bitmap);
